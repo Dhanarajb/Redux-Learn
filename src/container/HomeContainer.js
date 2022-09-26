f82a4eb5241012807cd6928fbdf5c810edc0f195
Dhanaraj <dhanarajbhaskar690@gmail.com>
2022-09-26 16:37:27 +0530

    add item to cart is complete

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import {connect} from 'react-redux'
 import Home from '../components/Home'
+import { addToCart } from '../Services/Actions/Action'
 
-const HomeContainer = () => {
-  return (
-    <div>
-        <Home />
-    </div>
-  )
-}
+const mapStateToProps=state=>({
 
-export default HomeContainer
+})
+const mapDispatchToProps = dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
